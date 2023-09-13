9c7ef778206b8f2d6ba5ce56e283a130df1f9388
benicastro <benedict.zcastro@gmail.com>
2023-09-13 23:23:37 +0800

    Simulated a random walk process

--- random_walk.py
+++ random_walk.py
@@ -1,29 +1,33 @@
-# NumPy is imported, seed is set
+# Import numpy and matplotlib
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.random.seed(123)
 
-# Initialization
-random_walk = [0]
+# Simulate random walk 500 times
+all_walks = []
+for i in range(500):
+    random_walk = [0]
+    for x in range(100):
+        step = random_walk[-1]
+        dice = np.random.randint(1, 7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1, 7)
+        if np.random.rand() <= 0.001:
+            step = 0
+        random_walk.append(step)
+    all_walks.append(random_walk)
 
-for x in range(100):
-    step = random_walk[-1]
-    dice = np.random.randint(1, 7)
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
 
-    if dice <= 2:
-        step = max(0, step - 1)
-    elif dice <= 5:
-        step = step + 1
-    else:
-        step = step + np.random.randint(1, 7)
+# Select last row from np_aw_t: ends
+ends = np_aw_t[-1, :]
 
-    random_walk.append(step)
-
-# Import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
-
-# Plot random_walk
-plt.plot(random_walk)
-
-# Show the plot
+# Plot histogram of ends, display plot
+plt.hist(ends)
 plt.show()
